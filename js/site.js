f951fda2b0ac729c5b8080cedaaf3b6622d90670
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2021-11-08 17:17:46 -0500

    building javascript app

--- js/site.js
+++ js/site.js
@@ -1,4 +1,40 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the string from the page.
+//Controller function.
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementsByID("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string.
+//Logic function.
+function reverseString(userString) {
+    
+    let revString = [];
+
+    /*
+
+    let name = Carlos
+    name[0] = C;
+    name[5] = s;
+
+    */
+    //Reverse a string using a for loop.
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+
+}
+
+//Display the reversed string to the user.
+//View function.
+function displayString() {
+    
 }
