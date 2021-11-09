a16612167f50ecf8019c9980c7804a64387028d1
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2021-11-09 08:42:37 -0500

    Working on javascript

--- js/site.js
+++ js/site.js
@@ -16,25 +16,31 @@ function getValue() {
 function reverseString(userString) {
     
     let revString = [];
-
     /*
-
     let name = Carlos
     name[0] = C;
     name[5] = s;
-
+    name.length - 1 (This will find the last position in the array)
     */
-    //Reverse a string using a for loop.
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
-    }
 
+    //Reverse a string using a for loop.
+    //This is a decrementing for loop.
+    for (let index = userString.length - 1; index >= 0; index--) {
+    //+= will add to revString each iterantion of the loop until it gets to 0
+        revString += userString[index];        
+    }
+    //Once the loop is complete it will return revString to the controlling function.
+    return revString;
 
 }
 
 //Display the reversed string to the user.
 //View function.
-function displayString() {
-    
+function displayString(revString) {
+
+    //Write to the page.
+                                                //This is a temerate literal.
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //Show the alert box.
+    document.getElementById("alert").classList.remove("invisible");
 }
